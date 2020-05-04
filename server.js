deab125ae93c7b3fcdd17e4848c99cd0b927f0c5
Rishab Gupta <gupta.rishab17@gmail.com>
2020-05-04 06:06:04 +0530

    added appserver

---
+++ server.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv')
+dotenv.config({ path: './.env' })
+
+const logger = require('./logger')
+const server = require('./app')
+
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
+    logger.log('info', `Application is running on ${port}`)
+})
