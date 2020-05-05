4b617d3dee4b04940100e748598dfcaa515acab8
Rishab Gupta <gupta.rishab17@gmail.com>
2020-05-05 15:03:12 +0530

    corrected folder structure

--- express/server.js
+++ express/server.js
@@ -1,8 +1,8 @@
 const dotenv = require('dotenv')
-dotenv.config({ path: './.env' })
+dotenv.config({ path: './../.env' })
 
-const logger = require('./logger')
-const server = require('./app')
+const logger = require('../logger')
+const server = require('../app')
 
 const port = process.env.PORT || 3000
 
