7ecb89ea22a2751ef65fe418537f293e87420f7b
Rishab Gupta <gupta.rishab17@gmail.com>
2020-05-05 14:47:57 +0530

    netlify lambda

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const serverless = require('serverless-http')
 const app = express()
 
 app.use(express.static(path.resolve('build')))
@@ -8,3 +9,4 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve('build/index.html'))
 })
 module.exports = app
+module.exports.handler = serverless(app)
